cc45a587a7b8e91f1141f8cd9b9e07cd45f98a57
diego.martinez13 <diego.martinez13@upr.edu>
2024-04-25 15:26:16 -0400

    nametables

--- Task_4/src/st1.asm
+++ Task_4/src/st1.asm
@@ -16,51 +16,49 @@
 	LDA #$00
 	STA temp_1
 
-	FirstWorld:
-		LDA PPUSTATUS
-		LDA #$24
-		STA PPUADDR    
-		LDA #$00
-		STA PPUADDR  
+	LDA PPUSTATUS
+	LDA #$24
+	STA PPUADDR    
+	LDA #$00
+	STA PPUADDR  
 
-		OuterLoop:
-		Start:
-		LDY #$00
+	OuterLoop:
+	LDY #$00
 
-		LoopAgain:   
-		LDX temp
-		Loop:  
-			LDA nametable1,X       
-			STA PPUDATA 
-			LDA nametable1,X       
-			STA PPUDATA    
-			INX           
-			CPX temp_addr 
-			BNE Loop
-		INY
-		CPY #$02
-		BNE LoopAgain
-		
-		LDA temp_addr       ; Load the low byte of ztemp
-		CLC             ; Clear the carry flag before addition
-		ADC #$10      ; Add 30 (1E in hex) to the accumulator
-		STA temp_addr       ; Store the result back in ztemp
+	LoopAgain:   
+	LDX temp
+	Loop:  
+		LDA nametable1,X       
+		STA PPUDATA 
+		LDA nametable1,X       
+		STA PPUDATA    
+		INX           
+		CPX temp_addr 
+		BNE Loop
+	INY
+	CPY #$02
+	BNE LoopAgain
+	
+	LDA temp_addr       ; Load the low byte of ztemp
+	CLC             ; Clear the carry flag before addition
+	ADC #$10      ; Add 30 (1E in hex) to the accumulator
+	STA temp_addr       ; Store the result back in ztemp
 
-		LDA temp_addr+1     ; Load the high byte of ztemp
-		ADC #$00        ; Add any carry from the previous addition
-		STA temp_addr+1     ; Store the result back in ztemp+1
+	LDA temp_addr+1     ; Load the high byte of ztemp
+	ADC #$00        ; Add any carry from the previous addition
+	STA temp_addr+1     ; Store the result back in ztemp+1
 
-		STX temp
+	STX temp
 
-		LDA temp_1
-		CLC       ; Clear the carry flag to ensure clean addition
-		ADC #$01  ; Add with carry the value 1 to the accumulator
-		STA temp_1
+	LDA temp_1
+	CLC       ; Clear the carry flag to ensure clean addition
+	ADC #$01  ; Add with carry the value 1 to the accumulator
+	STA temp_1
 
-		CMP #$0F 
-		BEQ END
+	CMP #$0F 
+	BEQ END
 
-		JMP OuterLoop
+	JMP OuterLoop
 
   END:
 	LDX #$00
@@ -70,13 +68,13 @@
 	LDA #$C0         ; Low byte of $23C0
 	STA PPUADDR
 
-    load_attributes:
-    LDX #%01010101
-    STX PPUDATA
-    CLC
-    ADC #$01
-    CMP #$00
-    BNE load_attributes
+	load_attributes:
+	LDX #%01010101
+	STX PPUDATA
+	CLC
+	ADC #$01
+	CMP #$00
+	BNE load_attributes
   
   RTS
 .endproc
